27a9b777d20e1715f2e982ac7d87739aa781c2da
penzai <pengzhen058@qq.com>
2020-11-12 14:35:08 +0800

    添加导出字段

--- src/components/CutVideo/index.vue
+++ src/components/CutVideo/index.vue
@@ -62,13 +62,22 @@ export default {
         this.$Message.error("视频地址有误，暂不能裁剪");
         return;
       }
-      const { cutAllTime, startTime, endTime } = this.$refs.content;
+      const {
+        cutAllTime,
+        startTime,
+        endTime,
+        startTimeText,
+        endTimeText
+      } = this.$refs.content;
       const all = Math.floor(cutAllTime);
       if (all < 7 || all > 48) {
         this.$Message.error("裁剪视频总时长需为7秒到48秒");
       } else {
         this.$emit("update:visible", false);
         this.$emit("confirm", {
+          cutAllTime,
+          cutBeginTimeText: startTimeText,
+          cutEndTimeText: endTimeText,
           cutBeginTime: Math.floor(startTime),
           cutEndTime: Math.floor(endTime)
         });
