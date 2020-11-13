e2f99b25b723101bb185da422a912d98be4a6fb7
penzai <pengzhen058@qq.com>
2020-11-13 10:30:28 +0800

    添加裁剪后视频展示

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,36 @@
   <div>
     <Button type="primary" @click="visible = true">打开</Button>
     <CutVideo url="/v2.mp4" :visible.sync="visible" @confirm="handleConfirm" />
+    <VideoShow v-if="show" src="/v2.mp4" :options="options" />
   </div>
 </template>
 
 <script>
 import CutVideo from "@/components/CutVideo";
+import VideoShow from "@/components/VideoShow";
 
 export default {
   components: {
-    CutVideo
+    CutVideo,
+    VideoShow
   },
 
   data() {
     return {
-      visible: false
+      visible: false,
+      show: false,
+      options: null
     };
   },
 
   methods: {
     handleConfirm(val) {
       console.log(val);
+      this.show = true;
+      this.options = {
+        start: val.cutBeginTime,
+        end: val.cutEndTime
+      };
     }
   }
 };
